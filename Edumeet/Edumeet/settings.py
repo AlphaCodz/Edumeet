1ccd4f89b945c1aa99666a41ebe715bc265a515e
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-14 22:16:20 +0100

    S

--- Edumeet/Edumeet/settings.py
+++ Edumeet/Edumeet/settings.py
@@ -16,11 +16,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
