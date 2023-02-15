dd9f0d41f64f10b6271541c5841eba757f4fb6ab
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-15 08:52:37 +0100

    Alpha

--- Edumeet/Edumeet/settings.py
+++ Edumeet/Edumeet/settings.py
@@ -35,7 +35,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["edumeet-production.up.railway.app"]
 # "edumeet-production.up.railway.app"
 
 # Application definition
